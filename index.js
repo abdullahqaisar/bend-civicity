1b919ff49f77520a4d32806675de38dd2dc300e6
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-10-15 22:17:56 +0500

    increased file size

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ mongoose
   });
   
 
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({limit: '25mb'}));
+
 app.use(cors());
 app.use(express.json());
 
