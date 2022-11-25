daba458710721c99028239ebde72d8723aa06abc
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-11-25 21:27:40 +0500

    added get user messages

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -23,3 +23,16 @@ exports.sendMessage = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+exports.getUserMessage = async (req, res) => {
+  try {
+    const userId = req.user;
+    const { to } = req.params;
+    const messages = await Message.find({
+      $or: [{ from: userId, to }, { from: to, to: userId }],
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
