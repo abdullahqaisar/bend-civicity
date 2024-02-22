88e4354ac560f4fb315f0615ec2c8439541cb57a
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-02-22 13:27:19 +0500

    Refactor car and user routes and controllers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,5 @@
 const User = require('../models/user.model');
 const Ride = require('../models/ride.model');
-const Car = require('../models/car.model');
 
 const { storeImage } = require('../helpers/storeImageToServer');
 
@@ -11,25 +10,6 @@ exports.getUser = async (req, res) => {
     return res.status(401).json({ message: 'Error finding user account' });
   }
   return res.status(201).send(user);
-};
-
-exports.uploadProfilePicture = async (req, res) => {
-  const userId = req.user;
-  const buffer = req.body.image;
-
-  const user = await User.findById({ _id: userId });
-  if (!user) {
-    return res.status(201).json({ message: 'Error finding user account' });
-  }
-
-  const { phoneNumber } = user;
-  const directory = `uploads/images/${phoneNumber}/profilepicture/`;
-  const imagePath = storeImage('profilepicture.png', buffer, directory);
-
-  user.ProfilePicture = imagePath;
-  await user.save();
-
-  return res.status(201).json({ message: 'Profile Picture Uploaded' });
 };
 
 exports.verifyLicenseImage = async (req, res) => {
@@ -49,59 +29,6 @@ exports.verifyLicenseImage = async (req, res) => {
   await user.save();
 
   return res.status(201).json({ message: 'License Image Uploaded' });
-};
-
-exports.addCar = async (req, res) => {
-  const userId = req.user;
-
-  let user = await User.findById({ _id: userId });
-  if (!user) {
-    return res.status(401).json({ message: 'Error finding user account' });
-  }
-
-  const { licensePlateNumber, brand, modelName, modelYear, colour } = req.body;
-
-  let car = new Car({
-    licensePlateNumber,
-    brand,
-    modelName,
-    modelYear,
-    colour,
-  });
-
-  car = await car.save();
-
-  if (!car) {
-    return res.status(401).json({ message: 'Error adding car' });
-  }
-
-  if (user.userType !== true) {
-    user.userType = true;
-  }
-  user.driverData.cars.push(car.id);
-  user = await user.save();
-
-  if (!user) {
-    return res.status(500).json({
-      message: 'Error adding car!',
-    });
-  }
-
-  return res.status(201).json({
-    message: 'Car Added!',
-  });
-};
-
-exports.getAllAddedCar = async (req, res) => {
-  const userId = req.user;
-
-  // only select cars from user
-  const user = await User.findById({ _id: userId }).populate('driverData.cars');
-  if (!user) {
-    return res.status(500).json({ message: 'Error finding Driver' });
-  }
-
-  return res.status(201).json({ cars: user.driverData.cars });
 };
 
 // get average rating and all ratings
@@ -129,16 +56,25 @@ exports.getDriverRating = async (req, res) => {
   return res.status(200).json({ averageRating, ratings: driverRatings });
 };
 
-exports.getPublishedRides = async (req, res) => {
+exports.getRides = async (req, res) => {
   const userId = req.params.id;
-  const data = await Ride.find({ driver: userId });
-  if (!data) {
-    return res.status(201).json({
+  const { published } = req.query;
+
+  const filter = { driver: userId };
+
+  if (published !== undefined) {
+    filter.published = published;
+  }
+
+  const data = await Ride.find(filter);
+
+  if (!data || data.length === 0) {
+    return res.status(404).json({
       message: 'No rides found!',
     });
   }
 
-  return res.status(201).send(data);
+  return res.status(200).send(data);
 };
 
 // find all rides that are completed and have the user as a passenger
@@ -157,51 +93,58 @@ exports.getCompletedRides = async (req, res) => {
   return res.status(201).send(data);
 };
 
-exports.addBio = async (req, res) => {
-  const { bio } = req.body;
+exports.uploadProfilePicture = async (req, res) => {
   const userId = req.user;
-  const user = await User.findOneAndUpdate({ _id: userId }, { bio });
+  const buffer = req.body.image;
+
+  const user = await User.findById({ _id: userId });
   if (!user) {
+    return res.status(201).json({ message: 'Error finding user account' });
+  }
+
+  const { phoneNumber } = user;
+  const directory = `uploads/images/${phoneNumber}/profilepicture/`;
+  const imagePath = storeImage('profilepicture.png', buffer, directory);
+
+  user.ProfilePicture = imagePath;
+  await user.save();
+
+  return res.status(201).json({ message: 'Profile Picture Uploaded' });
+};
+
+exports.updateProfile = async (req, res) => {
+  const userId = req.user;
+  const { bio, smoking, music, pets } = req.query;
+
+  let updateData;
+
+  if (bio !== undefined) {
+    updateData = { bio };
+  } else if (
+    smoking !== undefined ||
+    music !== undefined ||
+    pets !== undefined
+  ) {
+    updateData = {
+      ...(smoking !== undefined || music !== undefined || pets !== undefined
+        ? { preferences: { smoking, music, pets } }
+        : {}),
+    };
+  } else {
     return res.status(400).json({
-      message: 'An Error occoured while adding bio!',
+      message: 'Invalid query parameters!',
     });
   }
 
-  return res.status(201).json({
-    message: 'Bio Added!',
-  });
-};
+  const user = await User.findOneAndUpdate({ _id: userId }, updateData);
 
-exports.addPreferences = async (req, res) => {
-  const userId = req.user;
-  const { smoking, music, pets } = req.body;
-  const user = await User.findOneAndUpdate(
-    { _id: userId },
-    {
-      preferences: {
-        smoking,
-        music,
-        pets,
-      },
-    },
-  );
   if (!user) {
     return res.status(500).json({
-      message: 'An Error occoured while adding preferences!',
+      message: 'An Error occurred while updating profile!',
     });
   }
 
   return res.status(201).json({
-    message: 'Preferences Added!',
+    message: 'Profile updated successfully!',
   });
-};
-
-exports.deleteCar = async (req, res) => {
-  const carId = req.params.carid;
-
-  const car = await Car.remove({ _id: carId });
-  if (!car) {
-    return res.status(500).json({ message: "Can't delete car!" });
-  }
-  return res.status(201).json({ message: 'Car deleted!' });
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,23 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 const UserController = require('../controllers/user.controller');
-const CarController = require('../controllers/car.controller');
 
 const auth = require('../middleware/auth');
 
-router.patch('/addbio', auth, UserController.addBio);
-router.patch('/addpreferences', auth, UserController.addPreferences);
-router.patch('/uploadprofilepicture', auth, UserController.uploadProfilePicture);
-
+router.get('/', auth, UserController.getUser);
 router.get('/completedrides', auth, UserController.getCompletedRides);
-router.get('/getuser', auth, UserController.getUser);
-router.get('/getpulishedrides/:id', auth, UserController.getPublishedRides);
-// router.patch("/bookride", auth, UserController.bookRide);
-router.delete('/deletecar/:carid', auth, UserController.deleteCar);
-router.post('/addcar', auth, UserController.addCar);
-router.get('/getallcars', auth, UserController.getAllAddedCar);
-router.get('/getdriverrating', auth, UserController.getDriverRating);
-router.get('/getcarbybrands/:carbrand', CarController.getCarsByBrand);
-router.get('/getcarbrands', CarController.getCarBrands);
+router.get('/rides/:id', auth, UserController.getRides);
+router.get('/rating', auth, UserController.getDriverRating);
+router.patch('/', auth, UserController.updateProfile);
+router.patch(
+  '/uploadprofilepicture',
+  auth,
+  UserController.uploadProfilePicture,
+);
 
 module.exports = router;
