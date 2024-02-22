b2dfe32c904af5cd2db7efd1dc1aaea509473d58
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-02-22 12:03:43 +0500

    refactor helper function

--- helpers/convertImageToBase64.js
+++ helpers/convertImageToBase64.js
@@ -1,14 +1,6 @@
-const path = require('path');
-var fs = require('fs');
-var fs = require('fs-extra');
+const fs = require('fs');
 
 module.exports.convertImage = async (image) => {
-  try {
-    const buffer = await fs.readFileSync(image, 'base64');
-    debug(`Convert Buffer is ${buffer}`);
-    return buffer;
-  } catch (e) {
-    debug(e.message);
-    return res.status(500).json({ error: e.message });
-  }
+  const buffer = await fs.readFileSync(image, 'base64');
+  return buffer;
 };
